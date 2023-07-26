e3ed129b26c3cdc086ad560e33f6383c2fc9487e
sicao1 <singacao85@gmail.com>
2023-07-25 22:31:25 -0500

    getting setup and testing to pull data

---
+++ js/app.js
@@ -0,0 +1,12 @@
+window.onload = (event) => {
+  fetch(
+    "https://api.nasa.gov/planetary/apod?api_key=B1DPFwe5OBjq2QkaewOMi7dA2ZffDLqVn6H23mEx"
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then(
+      (json) => console.log(json),
+      (err) => console.log(err)
+    );
+};
