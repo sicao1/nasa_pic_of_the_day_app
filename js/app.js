10bd2b954ab21b374cd9d1fcf430eca079d01b70
sicao1 <singacao85@gmail.com>
2023-07-29 09:46:34 -0500

    date, title, and explanation imported

--- js/app.js
+++ js/app.js
@@ -11,13 +11,30 @@ window.onload = (event) => {
       (json) => {
         console.log(json);
         console.log(json.url);
-        // need to delete line 10 and 11 once done
+        console.log(json.title);
+        console.log(json.date);
+        // need to delete lines aboveonce done
+        // Get image
         const imageUrl = json.url;
         const imgElement = document.createElement("img");
         imgElement.src = imageUrl;
-
         const moveImg = document.querySelector("#image-container");
         moveImg.append(imgElement);
+
+        // Get Title
+        const title = json.title;
+        const placeTitle = document.querySelector(".title-of-photo h2");
+        placeTitle.append(title);
+
+        // Get Date
+        const date = json.date;
+        const placeDate = document.querySelector(".date-choosen h2");
+        placeDate.append(date);
+
+        // Get Explanation
+        const explanation = json.explanation;
+        const placeExplanation = document.querySelector(".explanation p");
+        placeExplanation.append(explanation);
       },
       (err) => console.log(err)
     );
@@ -30,4 +47,6 @@ window.onload = (event) => {
     console.log(selectedDate);
   };
   storeDate();
+
+  //Getting Title of Photo
 };
